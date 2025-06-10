1e6dffca82d81d2ddd45ce5f61b3a995d9f56bb2
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-10 19:07:30 -0400

    Update TokenLaunchpad.jsx

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,5 +1,26 @@
+import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from '@solana/spl-token';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { Transaction, SystemProgram } from '@solana/web3.js';
 
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+    async function createMint(){
+        const lamports = await getMinimumBalanceForRentExemptMint(connection); 
+
+            const transaction = new Transaction().add(
+                SystemProgram.createAccount({
+                    fromPubkey: payer.publicKey,
+                    newAccountPubkey: keypair.publicKey,
+                    space: MINT_SIZE,
+                    lamports,
+                    programId,
+                }),
+                createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
+            );
+            transaction.partialSign(keypair);
+            wallet.signTransaction(transaction); 
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +33,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button className='btn' onClick={createMint}>Create a token</button>
     </div>
 }
