6663b85dd5b12ddc858912e46fb9f9611bdadba9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-10 02:01:51 -0400

    Add files via upload

---
+++ src/Airdrop.jsx
@@ -0,0 +1,10 @@
+
+
+export function Airdrop(){
+    return <div>
+        <input input="text" placeholder="Amount"></input>
+        <button>
+            Send Airdrop
+        </button>
+    </div>
+}
