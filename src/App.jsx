3316577be85056ef3f24a38ebb1acf6ca2debfa1
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-10 14:29:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import {  WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import '@solana/wallet-adapter-react-ui/styles.css';
-import { Airdrop } from './Airdrop';
-import { useMemo } from 'react';
-import {
-    PhantomWalletAdapter,  
-} from '@solana/wallet-adapter-wallets';
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
 
-export default function App() {
-    const endpoint = useMemo(() => 'https://solana-devnet.g.alchemy.com/v2/i9WpXCP_SX2QikFfKnXFmZT--rjRGo65', []);
-
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
-
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                  <WalletMultiButton></WalletMultiButton>
-                  <WalletDisconnectButton></WalletDisconnectButton>
-                    <Airdrop />
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    );
+function App() {
+  return (
+    <TokenLaunchpad></TokenLaunchpad>
+  )
 }
+
+export default App
