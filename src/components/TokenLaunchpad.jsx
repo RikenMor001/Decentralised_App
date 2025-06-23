c49b598f1628e6b1526fae375d8901b229096a03
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-23 15:51:50 -0400

    Update TokenLaunchpad.jsx

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,26 +1,20 @@
-import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from '@solana/spl-token';
-import { useWallet } from '@solana/wallet-adapter-react';
-import { Transaction, SystemProgram } from '@solana/web3.js';
 
 export function TokenLaunchpad() {
-    const wallet = useWallet();
-    async function createMint(){
-        const lamports = await getMinimumBalanceForRentExemptMint(connection); 
 
-            const transaction = new Transaction().add(
-                SystemProgram.createAccount({
-                    fromPubkey: payer.publicKey,
-                    newAccountPubkey: keypair.publicKey,
-                    space: MINT_SIZE,
-                    lamports,
-                    programId,
-                }),
-                createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
-            );
-            transaction.partialSign(keypair);
-            wallet.signTransaction(transaction); 
-    }
+    function creatToken(){
+        const name = document.getElementById("name").value;
+        const imageUrl = document.getElementById("imageUrl").value;
+        const symbol = document.getElementById("symbol").value;
+        const totalSupply = document.getElementById("totalSupply").value;
 
+        console.log({
+            name,
+            symbol,
+            imageUrl,
+            totalSupply
+        })
+    }    
+    
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -29,10 +23,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn' onClick={createMint}>Create a token</button>
+        <input id = "name" className='inputText' type='text' placeholder='Name'></input> <br />
+        <input id = "symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
+        <input id = "imageUrl" className='inputText' type='text' placeholder='Image URL'></input> <br />
+        <input id = "totalSupply" className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <button onClick={creatToken} className='btn'>Create a token</button>
     </div>
 }
