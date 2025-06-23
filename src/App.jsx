bbbd773c62b3a00cdba0f325d642d72ae3f93b8f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-23 15:51:33 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
 
+import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
 
 function App() {
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+    ],
+    []
+  );
+
   return (
-    <div style={{width: "100vw"}}>
-      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-        <WalletProvider wallets={[]} autoConnect>
-            <WalletModalProvider>
-              <div style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                padding: 20
-              }}>
-                <WalletMultiButton />
-                <WalletDisconnectButton />
-              </div>
-              <TokenLaunchpad></TokenLaunchpad>
-            </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
-    </div>
+    <ConnectionProvider endpoint='https://api.devnet.solana.com'>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: 20
+          }}>
+            <WalletMultiButton/>
+            <WalletDisconnectButton/>
+          </div>
+          <TokenLaunchpad></TokenLaunchpad>
+        </WalletModalProvider>
+      </WalletProvider>      
+    </ConnectionProvider>
   )
 }
 
