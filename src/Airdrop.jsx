5ef4bb476bec1c353d18188f6e90b1d6f1edab13
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-23 15:51:12 -0400

    Update Airdrop.jsx

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,19 +1,1 @@
 
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-export function Airdrop(){
-
-    const wallet = useWallet();
-    const { connection } = useConnection();
-
-    async function SendAirdop(){
-        await connection.requestAirdrop(wallet.publicKey, 10000000);
-        alert("Airdrop Sent");
-    }
-
-    return <div>
-        <input input="text" placeholder="Amount"></input>
-        <button onClick={SendAirdop}>
-            Send Airdrop
-        </button>        
-    </div>
-}
