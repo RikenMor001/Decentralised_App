20190d3f84499a7ecdc0a3b05048767591041b86
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-23 16:37:20 -0400

    Update TokenLaunchpad.jsx

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,19 +1,41 @@
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID} from '@solana/spl-token'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
 
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
 
-    function creatToken(){
+    async function creatToken(){
         const name = document.getElementById("name").value;
         const imageUrl = document.getElementById("imageUrl").value;
         const symbol = document.getElementById("symbol").value;
         const totalSupply = document.getElementById("totalSupply").value;
 
         console.log({
-            name,
-            symbol,
-            imageUrl,
-            totalSupply
+            name, imageUrl, symbol, totalSupply
         })
-    }    
+        // createMint();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate();
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(keypair.publicKey, 6, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+        );
+
+        const recentBlockHash = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = recentBlockHash.blockhash;
+        transaction.feePayer = wallet.publicKey;
+
+        transaction.partialSign(keypair);
+        wallet.sendTransaction(transaction, connection);
+    }   
     
     return  <div style={{
         height: '100vh',
