2919987028cf0e03dcdc00cdb770ec90307f3fce
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-23 16:28:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID} from '@solana/spl-token'
+import { useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
 
-import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import '@solana/wallet-adapter-react-ui/styles.css';
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+export function TokenLaunchpad() {
+    const wallet = useWallet();
 
-function App() {
+    async function creatToken(){
+        const name = document.getElementById("name").value;
+        const imageUrl = document.getElementById("imageUrl").value;
+        const symbol = document.getElementById("symbol").value;
+        const totalSupply = document.getElementById("totalSupply").value;
 
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-    ],
-    []
-  );
+        console.log({
+            name, imageUrl, symbol, totalSupply
+        })
+        // createMint();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate();
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(keypair.publicKey, 6, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+        );
 
-  return (
-    <ConnectionProvider endpoint='https://api.devnet.solana.com'>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: 20
-          }}>
-            <WalletMultiButton/>
-            <WalletDisconnectButton/>
-          </div>
-          <TokenLaunchpad></TokenLaunchpad>
-        </WalletModalProvider>
-      </WalletProvider>      
-    </ConnectionProvider>
-  )
+        transaction.partialSign(keypair);
+        wallet.sendTransaction(transaction);
+    }   
+    
+    return  <div style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }}>
+        <h1>Solana Token Launchpad</h1>
+        <input id = "name" className='inputText' type='text' placeholder='Name'></input> <br />
+        <input id = "symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
+        <input id = "imageUrl" className='inputText' type='text' placeholder='Image URL'></input> <br />
+        <input id = "totalSupply" className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <button onClick={creatToken} className='btn'>Create a token</button>
+    </div>
 }
-
-export default App
